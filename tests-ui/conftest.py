a4526e3775f6346f308c4ebddf11a57a807d38ae
boss of the gym <90260538+Van-DarkHo1mes@users.noreply.github.com>
2026-03-27 20:31:30 +0300

    add github actions for ui and postman tests

--- tests-ui/conftest.py
+++ tests-ui/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 BASE_URL = "http://127.0.0.1:5500"
 API_URL = "http://localhost:3000/api"
@@ -9,12 +10,19 @@ API_URL = "http://localhost:3000/api"
 
 @pytest.fixture
 def driver():
-    requests.post(f"{API_URL}/test/reset")
+    try:
+        requests.post(f"{API_URL}/test/reset", timeout=2)
+    except:
+        pass
 
     options = Options()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
+
     driver.get(BASE_URL)
     yield driver
     driver.quit()
